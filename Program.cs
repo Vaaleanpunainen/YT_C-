7aa8538d4d8e119ed04a19f3d3c7f365188f895b
Ewelina Backtrog <oyfg@o2.pl>
2019-10-29 20:08:09 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator (+, -, * or /): ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("You entered a wrong operator");
+            }
+        }
+        
+    }
+}
